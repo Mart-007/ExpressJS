2f4b9f26a49b80ade0590b44ba6dfe52adec90e9
Reymart Bordo <62002257+Mart-007@users.noreply.github.com>
2021-05-25 15:10:19 +0800

    no updates GNG

--- Great_Number_Game/app.js
+++ Great_Number_Game/app.js
@@ -1,10 +1,54 @@
+const { request } = require('express');
 const express = require('express');
 const session = require('express-session');
 
 const app = express();
+app.use(express.urlencoded({extended: true}));
 
 app.use(express.static(__dirname + "/assets"));
 
 app.set('veiws', __dirname + '/views');
 app.set("view engine", 'ejs');
 
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge : 60000}
+}));
+
+
+app.get('/', (req, res) => {
+    let randNum = Math.floor(Math.random() * 100 + 1);
+    if(!req.session.number){
+        req.session.number = randNum;
+    }
+    if(!req.session.status){
+        req.session.status = ' ';
+    }
+    res.render('index', { guess: req.session.status });
+})
+
+app.post('/number', (req, res) => {
+    if(req.body.guess < req.session.number){
+        req.session.status = "low";
+    }
+    else if(req.body.guess > req.session.number){
+        req.session.status = "high";
+    }
+    else{
+        request.session.status = "correct";
+    }
+    res.redirect('/');
+})
+
+app.get('/reset', (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
+})
+
+
+
+app.listen(7000, () => {
+    console.log('Server running on port 7000');
+})
